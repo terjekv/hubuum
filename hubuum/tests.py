ab1dfd6a3abcf4d1b761b8ee4fc785af05d2e660
Terje Kvernes <terje@kvernes.no>
2023-01-05 19:09:51 +0100

    A build and test environment
     - A proper build and deploy setup for django apps
     - Tox testing support.
    - A github action that maybe (?) runs tox properly?

--- hubuum/tests.py
+++ hubuum/tests.py
@@ -1,8 +1,39 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, Group
 
 from .models import Host
 
 # Create your tests here.
+
+
+class UserAndGroupTestCase(TestCase):
+    """This class defines the test suite for the User and Group models."""
+
+    def setUp(self) -> None:
+        self.username = "tester"
+        self.password = "testpassword"
+
+        self.groupname = "testgroup"
+
+    def test_can_create_user(self):
+        self.user = User.objects.create(username=self.username, password=self.password)
+        self.assertIsNotNone(self.user)
+
+    def test_can_create_group(self):
+        self.group = Group.objects.create(name=self.groupname)
+        self.assertIsNotNone(self.group)
+
+    def test_can_assign_group_to_user(self):
+        self.user = User.objects.create(username=self.username, password=self.password)
+        self.assertIsNotNone(self.user)
+        self.group = Group.objects.create(name=self.groupname)
+        self.assertIsNotNone(self.group)
+        self.user.groups.set([self.group])
+        self.assertEqual(self.user.groups.all()[0].name, self.groupname)
+        self.assertCountEqual(self.user.groups.all(), [self.group])
+
+
+# login = self.client.login(username="testuser", password="12345")
 
 
 class HostTestCase(TestCase):
@@ -15,13 +46,26 @@ class HostTestCase(TestCase):
     def setUp(self):
         self.hostname = "testhost"
         self.serial = "test_serial_1"
+        self.username = "tester"
+        self.password = "testpassword"
+        self.groupname = "testgroup"
+
+        self.user = User.objects.create(username=self.username, password=self.password)
+        self.assertIsNotNone(self.user)
+
+        self.group = Group.objects.create(name=self.groupname)
+        self.assertIsNotNone(self.group)
 
     def test_can_create_host(self):
-        host = Host.objects.create(name=self.hostname, serial=self.serial)
+        host = Host.objects.create(
+            name=self.hostname, serial=self.serial, owner=self.group
+        )
         self.assertIsNotNone(host)
         self.assertIsInstance(host, Host)
 
     def test_create_has_correct_values(self):
-        host = Host.objects.create(name=self.hostname, serial=self.serial)
+        host = Host.objects.create(
+            name=self.hostname, serial=self.serial, owner=self.group
+        )
         self.assertEqual(host.serial, self.serial)
         self.assertEqual(host.name, self.hostname)
--- hubuumsite/settings.py
+++ hubuumsite/settings.py
@@ -118,6 +118,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -146,7 +158,8 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_L10N = True
+# Deprecated.
+# USE_L10N = True
 
 USE_TZ = True
 
