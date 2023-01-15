7784ed2868a6273c046c8b7b3a3abc7a3a58de2f
Terje Kvernes <terje@kvernes.no>
2023-01-15 04:33:51 +0100

    refactor: URL cleanup.

--- hubuumsite/urls.py
+++ hubuumsite/urls.py
@@ -4,12 +4,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.schemas import get_schema_view
+from rest_framework.permissions import AllowAny
 
-schema_view = get_schema_view(title="hubuum API")
+schema_view = get_schema_view(
+    title="hubuum API",
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("hubuum.api.v1.urls")),
-    path("", include("hubuum.api.urls")),
+    path("api/v1/", include("hubuum.api.v1.urls")),
+    path("api/", include("hubuum.api.urls")),
     path("docs/", schema_view),
 ]
